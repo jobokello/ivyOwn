54e7e724f16c1a2abb88ea7a849560a07fc9deef
jobokello <jobokello5@gmail.com>
2018-10-26 18:13:10 +0300

    ivymigrate

--- php/shopperReg.php
+++ php/shopperReg.php
@@ -1,4 +1,5 @@
 <?php
+//gives variable for creating the connection
 $servername = "localhost";
 $username = "root";
 $password = "";
@@ -7,27 +8,61 @@ $dbname = "ivyproject";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
+// Check  whether the connection was successful
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 echo "Connected successfully";
 
-$name = trim($_POST["name"]);
-$email = trim($_POST["email"]);
-$pass = trim($_POST["password"]);
+if (isset($_POST['fName'])) 
+{ 
+	$shopperfName = trim($_POST["fName"]);
+} 
 
-$sql = "INSERT INTO shopperinfo (name, email, password)
-VALUES ('$name', '$email', '$pass')";
+if (isset($_POST['sName'])) 
+{ 
+	$shoppersName = trim($_POST["sName"]);
+} 
+
+if (isset($_POST['username'])) 
+{ 
+$shopperusername = trim($_POST["username"]); 
+} 
+
+if (isset($_POST['email'])) 
+{ 
+	$shopperemail = trim($_POST["email"]); 
+} 
+
+if (isset($_POST['phone'])) 
+{ 
+	$shopperPhonenumber = trim($_POST["phone"]); 
+} 
+
+if (isset($_POST['county'])) 
+{ 
+	$shopperCounty = trim($_POST["county"]); 
+} 
+
+if (isset($_POST['constituency'])) 
+{ 
+$shopperConstituency = trim($_POST["constituency"]);
+} 
+
+if (isset($_POST['password'])) 
+{ 
+	$shopperPassword = md5($_POST["password"]);
+} 
+
+$sql = "INSERT INTO shopperinfo (shopperfName, shoppersName,shopperusername, shopperemail, shopperPhonenumber, shopperCounty, shopperConstituency, shopperPassword)
+VALUES ('$shopperfName', '$shoppersName', '$shopperusername', '$shopperemail', '$shopperPhonenumber', '$shopperCounty', '$shopperConstituency', '$shopperPassword')";
 
 if (mysqli_query($conn, $sql)) {
     echo "New record created successfully";
+    header('Location: ../html/shopperSuccess.html');
 } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
 }
 
 mysqli_close($conn);
-
-
-
 ?>
